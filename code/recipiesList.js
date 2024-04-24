256b18783c10acf48df9b121c6753cdc97975f69
Eli Zlatkova <eli.zlatkova@abv.bg>
2024-04-24 16:14:26 +0300

    list of recipies - the first 20 recipies added

--- code/recipiesList.js
+++ code/recipiesList.js
@@ -10,44 +10,79 @@ let ArrayOfMeals_IdTitleDescription = [];
 //list of titles to sort all the meals in alphabetical order
 let mealTitles = [];
 
-for (let j = 0; j < alphabet.length; j++) {
-    const URLletter = urlByFirstLetter + alphabet[j];
-    fetch(URLletter)
-        .then(resp => {
-            if (resp.status == 200) { return resp.json(); }
-        })
-        .then(data => {
-            if (data.meals != null) {
-                for (let i = 0; i < data.meals.length; i++) {
-                    const meal = data.meals[i];
-                    mealTitles.push(meal.strMeal);
-                    if (meal.strTags != null) {
-                        const obj = {
-                            'id': meal.idMeal,
-                            'title': meal.strMeal,
-                            'category': meal.strCategory,
-                            'area': meal.strArea,
-                            'tags': meal.strTags.split(",")
+async function getMealsObjandTitles() {
+    for (let j = 0; j < alphabet.length; j++) {
+        const URLletter = urlByFirstLetter + alphabet[j];
+        await fetch(URLletter)
+            .then(resp => {
+                if (resp.status == 200) { return resp.json(); }
+            })
+            .then(data => {
+                if (data.meals != null) {
+                    for (let i = 0; i < data.meals.length; i++) {
+                        const meal = data.meals[i];
+                        mealTitles.push(meal.strMeal);
+                        if (meal.strTags != null) {
+                            const obj = {
+                                'id': meal.idMeal,
+                                'title': meal.strMeal,
+                                'image': meal.strMealThumb,
+                                'category': meal.strCategory,
+                                'area': meal.strArea,
+                                'tags': meal.strTags.split(",")
+                            }
+                            ArrayOfMeals_IdTitleDescription.push(obj);
+                        } else {
+                            const obj = {
+                                'id': meal.idMeal,
+                                'title': meal.strMeal,
+                                'image': meal.strMealThumb,
+                                'category': meal.strCategory,
+                                'area': meal.strArea
+                            }
+                            ArrayOfMeals_IdTitleDescription.push(obj);
                         }
-                        ArrayOfMeals_IdTitleDescription.push(obj);
-                    } else {
-                        const obj = {
-                            'id': meal.idMeal,
-                            'title': meal.strMeal,
-                            'category': meal.strCategory,
-                            'area': meal.strArea
-                        }
-                        ArrayOfMeals_IdTitleDescription.push(obj);
                     }
-                    ListOfMeals.innerHTML += meal.strMeal + "<br>";
                 }
-            }
-        })
-        .catch(err => console.log(err.message));
+            })
+            .catch(err => console.log(err.message));
+    }
+    return ArrayOfMeals_IdTitleDescription
 }
 
-setTimeout(() => {
-    console.log(ArrayOfMeals_IdTitleDescription);
-    mealTitles.sort();
-    console.log(mealTitles);
-}, "6000");
+getMealsObjandTitles()
+    .then(() => {
+        console.log(ArrayOfMeals_IdTitleDescription);
+        mealTitles.sort();
+        console.log(mealTitles);
+        for (let i = 0; i < 20; i++) {
+            if(ArrayOfMeals_IdTitleDescription[i].tags){
+                ListOfMeals.innerHTML += `
+                <div class="individualMeal">
+                    <img class="imageIndividualMeal" src="${ArrayOfMeals_IdTitleDescription[i].image}" alt="image of the meal">
+                    <h2>${ArrayOfMeals_IdTitleDescription[i].title}</h2>
+                    <p>
+                        Area: ${ArrayOfMeals_IdTitleDescription[i].area}<br>
+                        Category: ${ArrayOfMeals_IdTitleDescription[i].category}<br>
+                        Tags: ${ArrayOfMeals_IdTitleDescription[i].tags}<br>
+                    </p>
+                </div>
+                <br>
+                `;
+            } else {
+                ListOfMeals.innerHTML += `
+                <div class="individualMeal">
+                    <img class="imageIndividualMeal" src="${ArrayOfMeals_IdTitleDescription[i].image}" alt="image of the meal">
+                    <h2>${ArrayOfMeals_IdTitleDescription[i].title}</h2>
+                    <p>
+                        Area: ${ArrayOfMeals_IdTitleDescription[i].area}<br>
+                        Category: ${ArrayOfMeals_IdTitleDescription[i].category}<br>
+                    </p>
+                </div>
+                <br>
+                `;
+            }
+            
+        }
+
+    })
